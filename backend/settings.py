14a646488fc8265b0acea28ad9ac555c96c49d3c
Senyo197 <Senyo197@gmail.com>
2024-03-21 05:38:40 +0000

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -74,8 +74,7 @@ CORS_ALLOWED_ORIGINS = [
 '''
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1',
+    'https://trading-pulse.vercel.app',
     'http://0.0.0.0',
 ]
 
