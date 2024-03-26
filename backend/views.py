c3c7b15587fdba1869f4a9ddf5914e2bcd7f64e0
Senyo197 <Senyo197@gmail.com>
2024-03-26 05:28:05 +0000

    Done

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,12 @@
+from api.models import EconomicEvent
+from api.serializers import EconomicEventSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view()
+@api_view(['GET'])
 def root_route(request):
-    return Response({
-        "message": "Welcome to Economic events API!"
-    })
+    if request.method == 'GET':
+        events = EconomicEvent.objects.all()  # Retrieve all records from the database
+        serializer = EconomicEventSerializer(events, many=True)  # Serialize the queryset
+        return Response(serializer.data)
+
